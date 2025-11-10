78eb42f4427290e4aa8cfbffb75c3b76cf4ae915
Alexy Lafosse <149681987+XLA17@users.noreply.github.com>
2025-11-10 15:30:56 +0100

    add npc command basic

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,54 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class NPC : MonoBehaviour
+{
+    [SerializeField] private List<Consumable> consumablesAsked;
+    [SerializeField] private TextMeshPro commandText;
+
+    private List<string> command;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        commandText.gameObject.SetActive(false);
+        AskFood();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AskFood()
+    {
+        Debug.Log("NPC ask food");
+        commandText.gameObject.SetActive(true);
+        command = new();
+        command.Add(consumablesAsked[0].getObjectName());
+        // TODO: activeBulle
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collide");
+        if (other.transform.parent.TryGetComponent<Consumable>(out Consumable c))
+        {
+            if (command.Contains(c.getObjectName()))
+            {
+                command.Remove(c.getObjectName());
+                if (command.Count == 0)
+                {
+                    commandText.gameObject.SetActive(false);
+                    Debug.Log("command finished");
+                }
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
