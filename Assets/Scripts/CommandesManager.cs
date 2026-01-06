1eb3617382ccd851687fbed275bf184a2b94c531
Maxime coux <100847666+mcoux@users.noreply.github.com>
2026-01-06 15:32:41 +0100

    Ajout colère

--- Assets/Scripts/CommandesManager.cs
+++ Assets/Scripts/CommandesManager.cs
@@ -35,20 +35,42 @@ public class CommandeManager: MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {            
+        
         int i = 0;
         timer+= Time.deltaTime;
         if(timer >= TIME_INTERVAL)
         {
+            //Augmentation de la colère
+            //On le fait avant pour pas qu'une commande qui vient de spawn l'augmente direct
+            foreach (var spawnPoint in spawnPoints)
+            {
+                //Si la commande est active
+                if (spawnPoint.Item2)
+                {
+                    anger++;
+                }
+            }
+
+            //Spawn des commandes
             timer = 0;
+            //Check si on peut faire spawn plus de commandes
             if (nbCommandes<maxCommandes)
             {
+                //Check si on fait bien spawn une commande
                 if (Random.Range(0, 20) <= difficulty)
                 {
                     commande = Instantiate(commandePrefab);
-                    spawnPointsIndex = Random.Range(0, spawnPoints.Count);
+                    //On génere un index aléatoire jusqu'a trouver un emplacement libre
+                    do
+                    {
+                        spawnPointsIndex = Random.Range(0, spawnPoints.Count);
+
+                    } while (spawnPoints[spawnPointsIndex].Item2);
+                    //Dernier check (peut être à dégager)
                     if (!spawnPoints[spawnPointsIndex].Item2)
                     {
+                        //Setup de la commande
                         spawnPoints[spawnPointsIndex] = (spawnPoints[spawnPointsIndex].Item1, true);
 
                         newSpawnPoint2D = spawnPoints[spawnPointsIndex].Item1;
@@ -58,9 +80,11 @@ public class CommandeManager: MonoBehaviour
 
 
                         commande.transform.position = newSpawnPoint;
+                        //On lie l'Action de la commande
                         commande.GetComponent<NPC>().isCompleted += orderCompleted;
                         commande.GetComponent<NPC>().index = spawnPointsIndex;
 
+                        //Rotation de la commande vers le joueur
                         commande.transform.LookAt(new Vector3(player.transform.position.x,commande.transform.position.y,player.transform.position.z));
                         commande.transform.Rotate(new Vector3(0, 90, 0));
                         Debug.Log("Spawn");
@@ -72,7 +96,12 @@ public class CommandeManager: MonoBehaviour
                     
                 }
             }
+
+            Debug.Log(anger);
         }
+
+
+
     }
 
     public void increaseAnger(float amount)
@@ -86,6 +115,7 @@ public class CommandeManager: MonoBehaviour
 
     private void orderCompleted(int index)
     {
+        //Modification de l'emplacement
         spawnPoints[index] = (spawnPoints[index].Item1, false);
         nbCommandes--;
     }
